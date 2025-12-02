5241a46ce468e9d1899fb5cf1b9d8a8c8b3febec
Devavrata Niraj <niraj.dev5@gmail.com>
2025-12-02 12:19:31 +0530

    Started Working on Controller Folder

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,71 @@
+import genToken from "../config/token.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signUp = async (req, res) => {
+  try {
+    const { name, email, password, userName } = req.body;
+
+    const findByEmail = await User.findOne({ email });
+    if (findByEmail) {
+      return res.status(400).json({ message: "Email Already Exists!" });
+    }
+
+    const findByUserName = await User.findOne({ userName });
+    if (findByUserName) {
+      return res.status(400).json({ message: "Username Already Exists!" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      userName,
+      email,
+      password: hashedPassword,
+    });
+
+    const token = await genToken(user._id);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 315360000000,
+      secure: false,
+      sameSite: "Strict",
+    });
+
+    return res.status(201).json(user);
+  } catch (error) {
+    return res.status(500).json({ message: `Signup Error ${error}` });
+  }
+};
+
+export const signIn = async (req, res) => {
+  try {
+    const { password, userName } = req.body;
+
+    const user = await User.findOne({ userName });
+    if (!user) {
+      return res.status(404).json({ message: "User Not Found!" });
+    }
+
+    const isMatch = bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Incorrect Password!" });
+    }
+
+    const token = await genToken(user._id);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 315360000000,
+      secure: false,
+      sameSite: "Strict",
+    });
+
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json({ message: `SignIn Error ${error}` });
+  }
+};
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -27,6 +27,8 @@ const userSchema = new mongoose.Schema(
     following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
     saved: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+    loops: [{ type: mongoose.Schema.Types.ObjectId, ref: "Loop" }],
+    story: [{ type: mongoose.Schema.Types.ObjectId, ref: "Story" }],
   },
   { timestamps: true }
 );
