6adfb680e3b59c63973580edb9a6d34f3773d222
Devavrata Niraj <niraj.dev5@gmail.com>
2025-12-07 17:34:29 +0530

    Designing SignUp Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 
 function App() {
   return (
@@ -8,6 +9,10 @@ function App() {
       <Route
         path="/signup"
         element={<SignUp />}
+      />
+      <Route
+        path="/signin"
+        element={<SignIn />}
       />
     </Routes>
   );
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,7 +1,120 @@
-import React from "react";
+import React, { useState } from "react";
+import logo from "../assets/Pixoverse-u.png";
+import { IoIosEye } from "react-icons/io";
+import { IoIosEyeOff } from "react-icons/io";
 
 function SignUp() {
-  return <div></div>;
+  const [inputClicked, setInputClicked] = useState({
+    name: false,
+    userName: false,
+    email: false,
+    password: false,
+  });
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <div className="w-full h-screen bg-gradient-to-b from-black to-gray-900 flex flex-col justify-center items-center">
+      <div className="w-[90%] lg:max-w-[60%] h-[600px] bg-zinc-400 rounded-2xl flex justify-center items-center overflow-hidden border-2 border-[#1a1f23]">
+        <div className="w-full lg:w-[50%] h-full bg-zinc-400 flex flex-col items-center p-[10px] gap-[20px]">
+          <div className="flex gap-[10px] items-center text-[20px] font-semibold mt-[14px]">
+            <span>Sign Up To</span>
+            <img
+              src={logo}
+              alt="Logo of PixoVerse"
+              className="w-[60px]"
+            />
+          </div>
+          <div
+            className="relative flex items-center justify-start w-[90%] h-[50px] rounded-2xl mt-[30px] border-2 border-black"
+            onClick={() => setInputClicked({ ...inputClicked, name: true })}>
+            <label
+              htmlFor="name"
+              className={`bg-zinc-400 absolute left-[20px] p-[5px] text-[15px] pointer-events-none ${
+                inputClicked.name ? "top-[-20px]" : ""
+              }
+             `}>
+              Enter Your Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="w-[100%] h-[100%] rounded-2xl px-[20px] outline-none border-0"
+              required
+            />
+          </div>
+          <div
+            className="relative flex items-center justify-start w-[90%] h-[50px] rounded-2xl mt-[30px] border-2 border-black"
+            onClick={() =>
+              setInputClicked({ ...inputClicked, userName: true })
+            }>
+            <label
+              htmlFor="userName"
+              className={`bg-zinc-400 absolute left-[20px] p-[5px]  text-[15px] pointer-events-none ${
+                inputClicked.userName ? "top-[-20px]" : ""
+              } `}>
+              Enter Your Username
+            </label>
+            <input
+              type="text"
+              id="userName"
+              className="w-[100%] h-[100%] rounded-2xl px-[20px] outline-none border-0"
+              required
+            />
+          </div>
+          <div
+            className="relative flex items-center justify-start w-[90%] h-[50px] rounded-2xl mt-[30px] border-2 border-black"
+            onClick={() => setInputClicked({ ...inputClicked, email: true })}>
+            <label
+              htmlFor="email"
+              className={`bg-zinc-400 absolute left-[20px] p-[5px]  text-[15px] 
+              pointer-events-none ${inputClicked.email ? "top-[-20px]" : ""}`}>
+              Enter Your Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="w-[100%] h-[100%] rounded-2xl px-[20px] outline-none border-0"
+              required
+            />
+          </div>
+          <div
+            className="relative flex items-center justify-start w-[90%] h-[50px] rounded-2xl mt-[30px] border-2 border-black"
+            onClick={() =>
+              setInputClicked({ ...inputClicked, password: true })
+            }>
+            <label
+              htmlFor="password"
+              className={`bg-zinc-400 absolute left-[20px] p-[5px] text-[15px] pointer-events-none ${
+                inputClicked.password ? "top-[-20px]" : ""
+              }
+              `}>
+              Enter Your Password
+            </label>
+            <input
+              type={showPassword ? "text" : "password"}
+              id="password"
+              className="w-[100%] h-[100%] rounded-2xl px-[20px] outline-none border-0"
+              required
+            />
+            {!showPassword ? (
+              <IoIosEye
+                className="absolute cursor-pointer right-[20px] w-[25px] h-[25px]"
+                onClick={() => setShowPassword(true)}
+              />
+            ) : (
+              <IoIosEyeOff
+                className="absolute cursor-pointer right-[20px] w-[25px] h-[25px]"
+                onClick={() => setShowPassword(false)}
+              />
+            )}
+          </div>
+          <button className="w-[70%] px-[20px] py-[10px] bg-black text-white font-semibold h-[50px] cursor-pointer rounded-2xl mt-[30px]">
+            Sign Up
+          </button>
+        </div>
+        <div className="md:w-[50%] h-full hidden lg:flex justify-center items-center bg-[#000000] flex-col gap-[10px] text-white text-[16px] font-semibold rounded-l-[30px] shadow-2xl shadow-black"></div>
+      </div>
+    </div>
+  );
 }
 
 export default SignUp;
